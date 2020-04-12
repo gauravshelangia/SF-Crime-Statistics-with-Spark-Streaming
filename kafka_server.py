f23513d2ea2e548758717027be7b77b2e82b9d5f
Gaurav Yadav <gaurav.yadav@webklipper.com>
2020-04-12 09:19:24 +0530

    Add results and code

--- kafka_server.py
+++ kafka_server.py
@@ -2,15 +2,15 @@ import producer_server
 
 
 def run_kafka_server():
-	# TODO get the json file path
-    input_file = ""
+    # TODO get the json file path
+    input_file = "police-department-calls-for-service.json"
 
     # TODO fill in blanks
     producer = producer_server.ProducerServer(
         input_file=input_file,
-        topic="",
-        bootstrap_servers="",
-        client_id=""
+        topic="police_calls-4",
+        bootstrap_servers="localhost:9092",
+        client_id="producer-3"
     )
 
     return producer
@@ -18,6 +18,7 @@ def run_kafka_server():
 
 def feed():
     producer = run_kafka_server()
+    print("Generate Feed")
     producer.generate_data()
 
 
--- producer_server.py
+++ producer_server.py
@@ -10,16 +10,22 @@ class ProducerServer(KafkaProducer):
         self.input_file = input_file
         self.topic = topic
 
-    #TODO we're generating a dummy data
+    # TODO we're generating a dummy data
     def generate_data(self):
-        with open(self.input_file) as f:
-            for line in f:
+        count = 0
+        with open(self.input_file, 'r') as file:
+            data = json.load(file)
+            print(len(data))
+            for line in data:
                 message = self.dict_to_binary(line)
                 # TODO send the correct data
-                self.send()
-                time.sleep(1)
+                self.send(self.topic, message)
+                count = count+1
+                if count % 30000 == 0:
+                    print("Event published = ", count)
+#                 time.sleep(1)
+        print("Number of call-message = ", count)
 
     # TODO fill this in to return the json dictionary to binary
     def dict_to_binary(self, json_dict):
-        return 
-        
+        return json.dumps(json_dict).encode("utf-8")
